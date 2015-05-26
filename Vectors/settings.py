098cf33334a502a5e3d11905d9f156ef07522c61
sodamodo <lamphead@gmail.com>
2015-05-26 15:06:35 -0400

    added relative path to templates

--- Vectors/settings.py
+++ Vectors/settings.py
@@ -100,6 +100,7 @@ STATICFILES_DIRS = (
 
 TEMPLATE_DIRS = (
     "C:/WebProjects/Vectors/templates",
+    os.path.join(BASE_DIR, "templates"),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
